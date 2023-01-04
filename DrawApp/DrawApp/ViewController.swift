233d030e75a276c7f8e70469445371df8b5d5a1d
dmiry <osipenko.minsk@gmail.com>
2023-01-04 15:38:16 +0300

    initial commit

---
+++ DrawApp/DrawApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawApp
+//
+//  Created by Дмитрий Осипенко on 4.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
